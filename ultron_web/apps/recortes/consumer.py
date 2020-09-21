25f37e231ec0abb872442d506dcf31ebbf44b9d2
Eduardo Basílio <eduardoafonsobasilio@gmail.com>
2020-09-21 10:45:58 -0300

    create consumer [wip]

--- ultron_web/apps/recortes/consumer.py
+++ ultron_web/apps/recortes/consumer.py
@@ -30,12 +30,12 @@ def main():
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
 
-if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        print('Interrupted')
-        try:
-            sys.exit(0)
-        except SystemExit:
-            os._exit(0)
+# if __name__ == '__main__':
+#     try:
+#         main()
+#     except KeyboardInterrupt:
+#         print('Interrupted')
+#         try:
+#             sys.exit(0)
+#         except SystemExit:
+#             os._exit(0)
